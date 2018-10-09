81237537ad302d68a9623837800ff930c79f52b9
denchou <denchou617@gmail.com>
2018-10-08 20:14:59 -0400

    Create Calendar template
    Create Interactive Calendar Class Component
    
    Renders - Month - Navigation bar to navigate between months
    Renders - Days of the week
    Renders - Individual day distinguishing between days belonging to month
    and not

--- src/BudgetCalendar.js
+++ src/BudgetCalendar.js
@@ -3,67 +3,128 @@ import dateFns from 'date-fns'
 import './BudgetCalendar.scss'
 
 class BudgetCalendar extends Component {
-  constructor () {
-    super()
+  constructor (props) {
+    super(props)
     // this creates a state of this month and time
     this.state = {
       currentMonth: new Date(),
-      selectedDate: new Date()
+      selectedDate: new Date(),
     }
   }
-  // method to select the next month using dateFns.addMonths
+  // render navigation header for the calendar
+  renderHeader() {
+    const dateFormat = 'MMMM YYYY'
+
+    return (
+      <div className="header row flex-middle">
+        <div className="col col-start">
+
+          <div className="icon" onClick={this.lastMonth}>
+            chevron_left
+          </div>
+        </div>
+        <div className="col col-center">
+          <span>{dateFns.format(this.state.currentMonth, dateFormat)}</span>
+        </div>
+        <div className="col col-end">
+          <div className="icon" onClick={this.nextMonth}>
+            chevron_right
+          </div>
+
+        </div>
+      </div>
+    )
+  }
+  // render named days of the week for the calendar
+  renderDays() {
+    const dateFormat = 'dddd'
+    const days = []
+
+    const startDate = dateFns.startOfWeek(this.state.currentMonth)
+
+    for (let i = 0; i < 7; i++) {
+      days.push(
+        <div className="col col-center" key={i}>
+          {dateFns.format(dateFns.addDays(startDate, i), dateFormat)}
+        </div>
+      )
+    }
+
+    return <div className="days row">{days}</div>
+  }
+  // render individual cells for the calendar
+  renderCells() {
+    const { currentMonth, selectedDate } = this.state
+    const monthStart = dateFns.startOfMonth(currentMonth)
+    const monthEnd = dateFns.endOfMonth(monthStart)
+    const startDate = dateFns.startOfWeek(monthStart)
+    const endDate = dateFns.endOfWeek(monthEnd)
+
+    const dateFormat = 'D'
+    const rows = []
+
+    let days = []
+    let day = startDate
+    let formattedDate = ''
+    // populates the day of the month, if the day is not in the same month
+    // it is greyed out.
+    while (day <= endDate) {
+      for (let i = 0; i < 7; i++) {
+        formattedDate = dateFns.format(day, dateFormat)
+        const cloneDay = day
+        days.push(
+          <div
+            className={`col cell ${
+              !dateFns.isSameMonth(day, monthStart)
+                ? 'disabled'
+                : dateFns.isSameDay(day, selectedDate) ? 'selected' : ''
+            }`}
+            key={day}
+            onClick={() => this.onDateClick(dateFns.parse(cloneDay))}
+          >
+            <span className="number">{formattedDate}</span>
+            <span className="bg">{formattedDate}</span>
+          </div>
+        )
+        day = dateFns.addDays(day, 1)
+      }
+      rows.push(
+        <div className="row" key={day}>
+          {days}
+        </div>
+      )
+      days = []
+    }
+    return <div className="body">{rows}</div>
+  }
+  // method to set current date to the date clicked
+  onDateClick = day => {
+    this.setState({
+      selectedDate: day
+    })
+  }
+  // method to select the next month in the calendar header
   nextMonth = () => {
     this.setState({
       currentMonth: dateFns.addMonths(this.state.currentMonth, 1)
     })
   }
-  // method to select the previous month using dateFns.subMonths
-lastMonth = () => {
-  this.setState({
-    currentMonth: dateFns.subMonths(this.state.currentMonth, 1)
-  })
-}
-// render the header for the calendar to navigate between months
-renderHeader() {
-  const dateFormat = 'MMMM YYYY'
-  return (
-    <div className="header row flex-middle">
-      <div className="col col-start">
-        <div className="icon" onClick={this.lastMonth}>
-          chevron_left
-        </div>
+  // method to select the previous month in the calendar header
+  lastMonth = () => {
+    this.setState({
+      currentMonth: dateFns.subMonths(this.state.currentMonth, 1)
+    })
+  }
+  // renders calendar
+  render() {
+    return (
+      <div className="calendar">
+        {this.renderHeader()}
+        {this.renderDays()}
+        {this.renderCells()}
       </div>
-      <div className="col col-center">
-        <span>
-          {dateFns.format(this.state.currentMonth, dateFormat)}
-        </span>
-      </div>
-      <div className="col col-end" onClick={this.nextMonth}>
-        <div className="icon">chevron_right</div>
-      </div>
-    </div>
-  )
+    )
+  }
 }
-
-  renderDays() {}
-
-  renderCells() {}
-
-onDateClick = day => {}
-
-nextMonth = () => {}
-
-prevMonth = () => {}
-render() {
-  return (
-    <div className="calendar">
-      {this.renderHeader()}
-      {this.renderDays()}
-      {this.renderCells()}
-    </div>
-  )
-}
-}
-
 
 export default BudgetCalendar
--- src/header/Header.js
+++ src/header/Header.js
@@ -25,7 +25,7 @@ const alwaysOptions = (
 
 const Header = ({ user }) => (
   <header className="main-header">
-    <h1>Uber, But For Taxis</h1>
+    <h1>Budget Calendar</h1>
     <nav>
       { user && <span>Welcome, {user.email}</span>}
       { user ? authenticatedOptions : unauthenticatedOptions }
