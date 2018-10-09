fff6495a5f0dc29039a54446a903d1a50a4de208
denchou <denchou617@gmail.com>
2018-10-09 13:33:11 -0400

    Refactored codes

---
+++ src/sidebar/Sidebar.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import Budget from '../budget/Budget'
+
+import './Sidebar.scss'
+
+const authenticatedOptions = (
+  <React.Fragment>
+    <Link to="/budgetcalendar">View Calendar</Link>
+    <Link to='/budget-form'>Budget</Link>
+  </React.Fragment>
+)
+
+const unauthenticatedOptions = (
+  <React.Fragment>
+  </React.Fragment>
+)
+
+const alwaysOptions = (
+  <React.Fragment>
+  </React.Fragment>
+)
+
+const Sidebar = ({ user }) => (
+  <header className="side-bar">
+    <nav>
+      { user ? authenticatedOptions : unauthenticatedOptions }
+      { alwaysOptions }
+    </nav>
+  </header>
+)
+
+export default Sidebar
